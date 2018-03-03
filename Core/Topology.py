93feed7b707b742eec280ecfd12f764a7a72d579
Emmanuel Gomez Soler <emmanuel.gomezsoler@gmail.com>
2018-03-03 16:33:57 -0500

    Creado un nuevo campo inputLayer en la clase topology

--- Core/Topology.py
+++ Core/Topology.py
@@ -1,5 +1,6 @@
 class Topology:
-    def __init__(self,layers,neuronsPerLayaers,activationFunctionPerLayers):
-        self.layers=layers
-        self.neuronsPerLayaers=neuronsPerLayaers
+    def __init__(self,inputLayer,layers,neuronsPerLayaers,activationFunctionPerLayers,):
+        self.inputLayer=inputLayer #Int neurons in the input layer
+        self.layers=layers #Int total of layers
+        self.neuronsPerLayaers=neuronsPerLayaers #Array Int neurons per layer
         self.activationFunctionPerLayers=activationFunctionPerLayers
